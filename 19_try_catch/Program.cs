3f172e067a51d4aa9b3552afb487868ffd48c56b
Vitor - PC <vitor.email2011@gmail.com>
2022-06-08 00:08:58 -0300

    try catch e Exceptions

---
+++ 19_try_catch/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Course
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+                Estrutura de exceções no DOTNET
+
+                Exception
+                    > SystemException
+                        >> IndexOutOfRangeException
+                        >> NullReferenceException
+                        >> InvalidCastException
+                        >> etc...
+                    > ApplicationException
+
+                ApplicationException: São criadas pelo DEV, são personalizadas
+                SystemException: São geradas pelo DOTNET, das bibliotecas do próprio DOTNET
+            */
+
+            try
+            {
+                System.Console.WriteLine(">>> Dentro do TRY");
+                int n1 = 10;
+                int result = n1 / 0;
+                System.Console.WriteLine(">>> Fim do TRY");
+            }
+            catch (DivideByZeroException e)
+            {
+                System.Console.WriteLine(">>> Capturou um erro de DIVISÃO POR ZEROS:");
+                System.Console.WriteLine(e);
+                System.Console.WriteLine(">>> Message: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                System.Console.WriteLine(">>> Capturou um erro:");
+                System.Console.WriteLine(e);
+                System.Console.WriteLine(">>> Message: " + e.Message);
+            }
+            finally
+            {
+                System.Console.WriteLine("FINALLY: Executa sempre, com ou sem erro!");
+            }
+        }
+    }
+}
